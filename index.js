2fae0bdd2ed84da1788f4df3f04ab3593521a3a6
Genevieve <awavieve@gmail.com>
2025-03-30 18:22:48 -0400

    Add cors for different origins

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors");
 const path = require('path');
 const cookieParser = require("cookie-parser");
 const sessions = require("express-session");
@@ -14,6 +15,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: ['http://localhost:5173', '']
+}));
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
